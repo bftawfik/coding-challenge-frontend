d9b14220a1622d523a22ad9ee2afc15a7bf4a096
bftawfik <bftawfik@gmail.com>
2021-07-10 13:51:21 +0200

    add postNewPost

--- src/services/index.js
+++ src/services/index.js
@@ -1,11 +1,21 @@
 import axios from "axios";
 
-const domainUrl = "https://jsonplaceholder.typicode.com/users";
+const domainUrl = "https://jsonplaceholder.typicode.com";
 
 const getUsers = async () => {
-  return await axios.get(domainUrl).then((res) => {
-    return res.data;
-  });
+  return await axios.get(`${domainUrl}/users`);
 };
 
-export { getUsers };
+const postNewPost = async (title, body, userId) => {
+  return await axios
+    .post(`${domainUrl}/posts`, {
+      title: title,
+      body: body,
+      userId: userId,
+    })
+    .then(function (response) {
+      console.log(response);
+    });
+};
+
+export { getUsers, postNewPost };
