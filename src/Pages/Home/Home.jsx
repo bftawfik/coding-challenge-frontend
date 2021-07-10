27433574f808837504b51c1e6c171e5364282731
bftawfik <bftawfik@gmail.com>
2021-07-10 13:54:19 +0200

    add NewPost to home

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,30 +2,40 @@ import React from "react";
 
 import { useSelector, useDispatch } from "react-redux";
 import { changeSelectedUser } from "../../redux/users/usersSlice";
+import { changePostData } from "../../redux/post/postSlice";
 import UserList from "../../Components/UserList/UserList";
 import UserPosition from "../../Components/UserPosition/UserPosition";
+import NewPost from "../../Components/NewPost/NewPost";
 
-const Home = () => {
+const Home = ({ submitNewPost }) => {
   const dispatch = useDispatch();
 
-  const stateUsers = useSelector((state) => state.users.value);
-  const stateSelectedUser = useSelector((state) => state.users.selected);
+  const { users, post } = useSelector((state) => state);
+
   const {
     address: { geo },
-  } = stateSelectedUser || { address: {} };
+  } = users.selected || { address: {} };
+
   return (
     <div>
       <header>Home header</header>
       <main>
         <UserList
-          users={stateUsers}
-          selectedUser={stateSelectedUser}
+          users={users.value}
+          selectedUser={users.selected}
           changeUser={(user) => dispatch(changeSelectedUser(user))}
         />
         <UserPosition
           markerData={
             geo && { lat: parseFloat(geo.lat), lng: parseFloat(geo.lng) }
           }
+        />
+        <NewPost
+          onDataChange={(data) => {
+            dispatch(changePostData(data));
+          }}
+          post={post}
+          onSubmit={(e) => submitNewPost(e, post.title, post.body, users.selected)}
         />
       </main>
       <footer>Home footer</footer>
