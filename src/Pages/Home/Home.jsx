0ce751d654bd22cf5105f7c4da613f41119decc8
bftawfik <bftawfik@gmail.com>
2021-07-10 09:13:15 +0200

    add the home

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { useSelector } from "react-redux";
+
+const Home = () => {
+  const users = useSelector((state) => state.users.value);
+  console.log(users);
+  return (
+    <div>
+      Home
+    </div>
+  );
+};
+
+export default Home;
