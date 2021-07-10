5697ed712ae48747aab9c205d6dc21370a13c544
bftawfik <bftawfik@gmail.com>
2021-07-10 11:02:12 +0200

    add UserPosition to home

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,13 +3,16 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { changeSelectedUser } from "../../redux/users/usersSlice";
 import UserList from "../../Components/UserList/UserList";
+import UserPosition from "../../Components/UserPosition/UserPosition";
 
 const Home = () => {
   const dispatch = useDispatch();
 
   const stateUsers = useSelector((state) => state.users.value);
   const stateSelectedUser = useSelector((state) => state.users.selected);
-  
+  const {
+    address: { geo },
+  } = stateSelectedUser || { address: {} };
   return (
     <div>
       <header>Home header</header>
@@ -18,6 +21,13 @@ const Home = () => {
           users={stateUsers}
           selectedUser={stateSelectedUser}
           changeUser={(user) => dispatch(changeSelectedUser(user))}
+        />
+        <UserPosition
+          markerData={
+            geo
+              ? { lat: parseFloat(geo.lat), lng: parseFloat(geo.lng) }
+              : undefined
+          }
         />
       </main>
       <footer>Home footer</footer>
