a02bc471d8e0d89055db56b6c0988269d6f9d81c
bftawfik <bftawfik@gmail.com>
2021-07-10 09:54:41 +0200

    add select user event

--- src/Components/UserList/UserList.jsx
+++ src/Components/UserList/UserList.jsx
@@ -1,8 +1,14 @@
-const UserList = ({ users }) => {
+const UserList = ({ users, selectedUser, changeUser }) => {
   return (
     <ul>
       {users.map((user) => (
-        <li key={user.id}>{user.name}</li>
+        <li key={user.id}>
+          {user.id === selectedUser?.id ? (
+            <span>{user.name}</span>
+          ) : (
+            <button onClick={()=> changeUser(user)}>{user.name}</button>
+          )}
+        </li>
       ))}
     </ul>
   );
