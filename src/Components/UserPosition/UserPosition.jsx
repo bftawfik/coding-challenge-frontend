03d80c4f8078ad838aec422690e1e903e071bef8
bftawfik <bftawfik@gmail.com>
2021-07-10 11:01:08 +0200

    add UserPosition component

---
+++ src/Components/UserPosition/UserPosition.jsx
@@ -0,0 +1,40 @@
+import { compose, withProps } from "recompose";
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker,
+} from "react-google-maps";
+
+const defaultProps = {
+  googleMapURL:
+    "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
+  loadingElement: <div style={{ height: `100%` }} />,
+  containerElement: <div style={{ height: `400px` }} />,
+  mapElement: <div style={{ height: `100%` }} />,
+  defaultCenter: { lat: -34.397, lng: 150.644 },
+  defaultZoom: 3,
+  markerZoom: 8,
+};
+
+const UserPosition = ({
+  markerData,
+  defaultCenter,
+  defaultZoom,
+  markerZoom,
+}) => (
+  <GoogleMap
+    defaultZoom={defaultZoom}
+    defaultCenter={defaultCenter}
+    center={markerData || defaultCenter}
+    zoom={markerData ? markerZoom : defaultZoom}
+  >
+    {markerData && <Marker position={markerData} />}
+  </GoogleMap>
+);
+
+export default compose(
+  withProps(defaultProps),
+  withScriptjs,
+  withGoogleMap
+)(UserPosition);
