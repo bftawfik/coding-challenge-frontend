53a5f8b37cb6ebc337ef72a18d03bc3e123c9116
bftawfik <bftawfik@gmail.com>
2021-07-11 10:57:55 +0200

    style users list

--- src/Components/UserList/UserList.jsx
+++ src/Components/UserList/UserList.jsx
@@ -1,16 +1,20 @@
+import classes from "./UserList.module.scss";
+
 const UserList = ({ users, selectedUser, changeUser }) => {
   return (
-    <ul>
-      {users.map((user) => (
-        <li key={user.id}>
-          {user.id === selectedUser?.id ? (
-            <span>{user.name}</span>
-          ) : (
-            <button onClick={()=> changeUser(user)}>{user.name}</button>
-          )}
-        </li>
-      ))}
-    </ul>
+    <div className={classes.UserList}>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.id === selectedUser?.id ? (
+              <span>{user.name}</span>
+            ) : (
+              <button onClick={() => changeUser(user)}>{user.name}</button>
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
