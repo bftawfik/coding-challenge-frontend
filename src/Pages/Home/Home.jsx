ebc81d472d133d50723ce4e312a610df78355da8
bftawfik <bftawfik@gmail.com>
2021-07-11 07:30:14 +0200

    add new post data to home

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { useSelector, useDispatch } from "react-redux";
 import { changeSelectedUser } from "../../redux/users/usersSlice";
@@ -8,13 +8,51 @@ import UserPosition from "../../Components/UserPosition/UserPosition";
 import NewPost from "../../Components/NewPost/NewPost";
 
 const Home = ({ submitNewPost }) => {
+  const [msgId, setMmsgId] = useState(-1);
+
   const dispatch = useDispatch();
-
   const { users, post } = useSelector((state) => state);
-
   const {
     address: { geo },
   } = users.selected || { address: {} };
+  const newPostFields = [
+    {
+      type: "input",
+      label: "title",
+    },
+    {
+      type: "textarea",
+      label: "body",
+      alias: "post",
+    },
+    {
+      type: "submit",
+      label: "submit",
+    },
+  ];
+
+  const validationMsgs = [
+    "Please enter a valid title.",
+    "Please enter a valid post.",
+    "Please select a user from the list.",
+  ];
+
+  const validateFormData = () => {
+    if (post.title.trim().length === 0) {
+      setMmsgId(0);
+      return false;
+    }
+    if (post.body.trim().length === 0) {
+      setMmsgId(1);
+      return false;
+    }
+    if (!users.selected) {
+      setMmsgId(2);
+      return false;
+    }
+    setMmsgId(-1);
+    return true;
+  };
 
   return (
     <div>
@@ -31,11 +69,20 @@ const Home = ({ submitNewPost }) => {
           }
         />
         <NewPost
+          newPostFields={newPostFields}
+          validationMsgs={validationMsgs}
+          msgId={msgId}
           onDataChange={(data) => {
             dispatch(changePostData(data));
           }}
           post={post}
-          onSubmit={(e) => submitNewPost(e, post.title, post.body, users.selected)}
+          onSubmit={(e) => {
+            e.preventDefault();
+            console.log("99");
+            if (validateFormData()) {
+              submitNewPost(e, post.title, post.body, users.selected);
+            }
+          }}
         />
       </main>
       <footer>Home footer</footer>
